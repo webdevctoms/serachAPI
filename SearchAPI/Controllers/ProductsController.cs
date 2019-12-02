23d19aed4e4b1c8c0d001c16123fcab3398e63b1
Michael Peterson <WebDev@ctoms.ca>
2019-12-02 15:07:37 -0700

    setup async post requests to elastic search

--- SearchAPI/Controllers/ProductsController.cs
+++ SearchAPI/Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -20,6 +22,7 @@ namespace SearchAPI.Controllers
     public class ProductsController : ControllerBase
     {
         private static readonly HttpClient client = new HttpClient();
+        
         private Shirts shirts = new Shirts();
         private Pants pants = new Pants();
         private GetConfig config = new GetConfig();
@@ -36,10 +39,13 @@ namespace SearchAPI.Controllers
         }
         
         [Route("createPants/{num}")]
-        public Array CreatePants(int num)
+        public async Task<Array> CreatePants(int num)
         {
+            SetHeaders();
             SinglePant[] pantArr = new SinglePant[num];
-            string[] pantJsonStrings = new string[num];
+            String[] pantJsonStrings = new string[num];
+
+            string postUrl = config.Url + "/products/_doc/";
             for (int i = 0;i < num; i++)
             {
                 pantArr[i] = pants.CreatePant();
@@ -53,13 +59,23 @@ namespace SearchAPI.Controllers
             {
                 int loopTime = DateTime.UtcNow.Millisecond;
                 Console.WriteLine($"Loop time {loopTime} index: {i} {pantJsonStrings[i]}");
-
-
-                Thread.Sleep(1000);
+                var response = await client.PostAsync(postUrl,new StringContent(pantJsonStrings[i],Encoding.UTF8,"application/json"));
+                Console.WriteLine($"{response}");
+                //Thread.Sleep(1000);
             }
             int afterTime = DateTime.UtcNow.Millisecond;
             Console.WriteLine("Time after for loop: {0}", afterTime);
             return pantArr;
+        }
+
+        private void SetHeaders()
+        {
+            
+            string authorization = config.Username + ":" + config.Pass;
+            byte[] byteAuth = Encoding.UTF8.GetBytes(authorization);
+            authorization = Convert.ToBase64String(byteAuth);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authorization);
+            
         }
         
     }
